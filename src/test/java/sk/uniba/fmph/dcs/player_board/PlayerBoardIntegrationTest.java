c462578156e078f7deb47d4a61d86d160d8f6b8e
pilatova <pilatokr@gmail.com>
2024-11-21 19:26:39 +0100

    add PlayerBoard integration test

--- src/test/java/sk/uniba/fmph/dcs/player_board/PlayerBoardIntegrationTest.java
+++ src/test/java/sk/uniba/fmph/dcs/player_board/PlayerBoardIntegrationTest.java
@@ -2,10 +2,12 @@ package sk.uniba.fmph.dcs.player_board;
 
 import org.junit.Test;
 import sk.uniba.fmph.dcs.stone_age.Effect;
+import sk.uniba.fmph.dcs.stone_age.EndOfGameEffect;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class PlayerBoardIntegrationTest {
     @Test
@@ -14,7 +16,7 @@ public class PlayerBoardIntegrationTest {
         PlayerBoard pb = tmp.getKey();
         PlayerBoardGameBoardFacade pbf = tmp.getValue();
 
-        Effect[] resources = new Effect[] { Effect.FOOD, Effect.WOOD, Effect.CLAY, Effect.STONE, Effect.GOLD };
+        Effect[] resources = new Effect[]{Effect.FOOD, Effect.WOOD, Effect.CLAY, Effect.STONE, Effect.GOLD};
         pbf.giveEffect(resources);
         assert pb.getPlayerResourcesAndFood().hasResources(resources);
 
@@ -25,13 +27,13 @@ public class PlayerBoardIntegrationTest {
         assert !pbf.feedTribe(List.of());
         assert pbf.doNotFeedThisTurn();
         assert pbf.feedTribe(List.of());
-        assert !pbf.takeResources(new Effect[] { Effect.FOOD });
+        assert !pbf.takeResources(new Effect[]{Effect.FOOD});
 
         int points = pb.addPoints(0);
         assert points == -10;
 
         pbf.newTurn();
-        pbf.giveEffect(new Effect[] { Effect.FOOD, Effect.FOOD, Effect.FOOD, Effect.STONE, Effect.GOLD });
+        pbf.giveEffect(new Effect[]{Effect.FOOD, Effect.FOOD, Effect.FOOD, Effect.STONE, Effect.GOLD});
         assert !pbf.hasFigures(6);
         assert !pbf.isTribeFed();
         assert !pbf.feedTribe(Arrays.asList(Effect.FOOD, Effect.FOOD, Effect.CLAY, Effect.STONE, Effect.GOLD));
@@ -39,8 +41,8 @@ public class PlayerBoardIntegrationTest {
 
         assert pbf.feedTribe(Arrays.asList(Effect.FOOD, Effect.FOOD, Effect.FOOD, Effect.STONE, Effect.GOLD));
         assert pb.getPlayerResourcesAndFood()
-                .hasResources(new Effect[] { Effect.WOOD, Effect.CLAY, Effect.STONE, Effect.GOLD });
-        assert !pb.getPlayerResourcesAndFood().hasResources(new Effect[] { Effect.FOOD });
+                .hasResources(new Effect[]{Effect.WOOD, Effect.CLAY, Effect.STONE, Effect.GOLD});
+        assert !pb.getPlayerResourcesAndFood().hasResources(new Effect[]{Effect.FOOD});
         assert pbf.isTribeFed();
 
         pbf.giveFigure();
@@ -53,19 +55,74 @@ public class PlayerBoardIntegrationTest {
         assert pbf.hasFigures(6);
         assert pbf.takeFigures(6);
 
-        pbf.giveEffect(new Effect[] { Effect.FOOD, Effect.FOOD, Effect.FOOD, Effect.FOOD, Effect.FOOD });
+        pbf.giveEffect(new Effect[]{Effect.FOOD, Effect.FOOD, Effect.FOOD, Effect.FOOD, Effect.FOOD});
         assert !pbf.feedTribeIfEnoughFood();
         assert !pbf.isTribeFed();
-        pbf.giveEffect(new Effect[] { Effect.FOOD });
+        pbf.giveEffect(new Effect[]{Effect.FOOD});
         assert pbf.feedTribeIfEnoughFood();
         assert pb.getPlayerResourcesAndFood()
-                .hasResources(new Effect[] { Effect.WOOD, Effect.CLAY, Effect.STONE, Effect.GOLD });
-        assert !pb.getPlayerResourcesAndFood().hasResources(new Effect[] { Effect.FOOD });
+                .hasResources(new Effect[]{Effect.WOOD, Effect.CLAY, Effect.STONE, Effect.GOLD});
+        assert !pb.getPlayerResourcesAndFood().hasResources(new Effect[]{Effect.FOOD});
 
         points = pb.addPoints(0);
         assert points == -10;
         pb.addEndOfGamePoints();
         points = pb.addPoints(0);
         assert points == (-10 + 4);
+        pb.addEndOfGamePoints();
+        points = pb.addPoints(0);
+        assert points == (-10 + 4);
+    }
+
+    @Test
+    public void defaultConfigurationTest() {
+        Map.Entry<PlayerBoard, PlayerBoardGameBoardFacade> tmp = PlayerBoardFactory.createDefaultPlayerBoard();
+        PlayerBoard pb = tmp.getKey();
+        PlayerBoardGameBoardFacade pbf = tmp.getValue();
+
+        Effect[] twelveFoods = new Effect[12];
+        Arrays.fill(twelveFoods, Effect.FOOD);
+        assert pb.getPlayerResourcesAndFood().hasResources(twelveFoods);
+        assert pb.getPlayerFigures().hasFigures(5);
+        assert !pb.getPlayerFigures().hasFigures(6);
+
+        assert !pbf.isTribeFed();
+        assert pbf.takeFigures(5);
+        assert !pbf.hasFigures(1);
+
+        pbf.giveEffect(new Effect[]{Effect.WOOD});
+        List<Effect> feed = Arrays.asList(Effect.FOOD, Effect.FOOD, Effect.FOOD, Effect.FOOD, Effect.WOOD);
+        assert !pbf.feedTribe(feed);
+        assert !pbf.isTribeFed();
+        assert pbf.feedTribeIfEnoughFood();
+        assert pbf.isTribeFed();
+
+        assert pbf.hasSufficientTools(0);
+        pb.getPlayerTools().addSingleUseTool(4);
+        int points = pb.addPoints(0);
+        assert points == 0;
+
+        assert !pbf.hasSufficientTools(5);
+        pb.getPlayerTools().addTool();
+        assert pbf.hasSufficientTools(5);
+
+        Optional<Integer> optInt = pbf.useTool(3);
+        assert optInt.isPresent() && optInt.get() == 4;
+        optInt = pbf.useTool(0);
+        assert optInt.isPresent() && optInt.get() == 1;
+
+        pb.getPlayerTools().addTool();
+        assert !pbf.hasSufficientTools(2);
+        optInt = pbf.useTool(0);
+        assert optInt.isEmpty();
+        assert pbf.hasSufficientTools(1);
+
+        pbf.giveEndOfGameEffect(new EndOfGameEffect[]{EndOfGameEffect.WRITING, EndOfGameEffect.ART});
+        pbf.giveEndOfGameEffect(new EndOfGameEffect[]{EndOfGameEffect.TOOL_MAKER});
+        pbf.giveEndOfGameEffect(new EndOfGameEffect[]{EndOfGameEffect.SHAMAN, EndOfGameEffect.SHAMAN});
+
+        pb.addEndOfGamePoints();
+        points = pb.addPoints(0);
+        assert points == 3 + 4 + 10;
     }
 }
